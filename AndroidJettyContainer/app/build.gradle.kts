b8b65ffa44f242436494c9f5965bef9e0f233d86
ivan <ipostu20000127@gmail.com>
2024-05-30 22:16:47 +0300

    Global cleanup

--- AndroidJettyContainer/app/build.gradle.kts
+++ AndroidJettyContainer/app/build.gradle.kts
@@ -30,12 +30,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    packagingOptions {
-        exclude("META-INF/spring.*")
-        exclude("META-INF/hk2-locator/default")
-        exclude("META-INF/license.txt")
-        exclude("META-INF/*.txt")
-    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -54,27 +48,6 @@ dependencies {
     implementation("org.eclipse.jetty:jetty-client:8.1.2.v20120308")
 
     implementation("javax.servlet:javax.servlet-api:3.0.1")
-    implementation("org.glassfish.jersey.containers:jersey-container-servlet-core:2.26")
-    implementation("org.glassfish.jersey.core:jersey-server:2.26")
-    implementation("org.glassfish.jersey.inject:jersey-hk2:2.26")
-    {
-        exclude(group = "javax.inject", module = "javax.inject")
-    }
-    implementation("javax.xml.stream:stax-api:1.0-2")
-    implementation("javax.xml.bind:jaxb-api:2.3.1")
-
-
-
-//    implementation("org.glassfish.jersey.ext:jersey-spring4:2.26")
-//    {
-//        exclude(group = "javax.inject", module = "javax.inject")
-//        exclude(group = "org.glassfish.hk2.external", module = "aopalliance-repackaged")
-//        exclude(group = "org.glassfish.hk2", module = "hk2-utils")
-//
-//    }
-//    implementation("org.glassfish.hk2:hk2-api:2.26")
-//    implementation("org.glassfish.hk2:hk2-locator:2.26")
-
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- webapp-parent/web-app/src/main/java/org/mortbay/ijetty/hello/HelloWorld.java
+++ webapp-parent/web-app/src/main/java/org/mortbay/ijetty/hello/HelloWorld.java
@@ -1,4 +1,5 @@
 package org.mortbay.ijetty.hello;
+
 import java.io.IOException;
 
 import javax.servlet.ServletConfig;
@@ -11,40 +12,37 @@ import javax.servlet.http.HttpServletResponse;
 
 
 /* ------------------------------------------------------------ */
-/** Hello Servlet 
- * 
+
+/**
+ * Hello Servlet
  */
-public class HelloWorld extends HttpServlet 
-{
+public class HelloWorld extends HttpServlet {
     String proofOfLife = null;
-    
+
     /* ------------------------------------------------------------ */
-    public void init(ServletConfig config) throws ServletException
-    {
-    	super.init(config);
-    	//to demonstrate it is possible
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        //to demonstrate it is possible
         Object o = config.getServletContext().getAttribute("com.ipostu.androidjettycontainer.contentResolver");
-        android.content.ContentResolver resolver = (android.content.ContentResolver)o;
-        android.content.Context androidContext = (android.content.Context)config.getServletContext().getAttribute("com.ipostu.androidjettycontainer.context");
+        android.content.ContentResolver resolver = (android.content.ContentResolver) o;
+        android.content.Context androidContext = (android.content.Context) config.getServletContext().getAttribute("com.ipostu.androidjettycontainer.context");
         proofOfLife = androidContext.getApplicationInfo().packageName;
     }
 
     /* ------------------------------------------------------------ */
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
     /* ------------------------------------------------------------ */
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         ServletOutputStream out = response.getOutputStream();
         out.println("<html>");
-        out.println("<h1>Hello From Servlet Land!</h1>");
-        out.println("Brought to you by: "+proofOfLife);
+        out.println("<h1>Hello 2 From Servlet Land!</h1>");
+        out.println("Brought to you by: " + proofOfLife);
         out.println("</html>");
         out.flush();
     }
-    
+
 }
