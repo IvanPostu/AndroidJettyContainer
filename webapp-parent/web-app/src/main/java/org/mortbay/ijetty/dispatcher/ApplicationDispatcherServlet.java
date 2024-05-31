4c7a14dde7a71dd76c3518cfdc9b20daaa215172
ivan <ipostu20000127@gmail.com>
2024-05-31 11:13:09 +0300

    Cleanup

--- webapp-parent/web-app/src/main/java/org/mortbay/ijetty/dispatcher/ApplicationDispatcherServlet.java
+++ webapp-parent/web-app/src/main/java/org/mortbay/ijetty/dispatcher/ApplicationDispatcherServlet.java
@@ -1,113 +1,31 @@
 package org.mortbay.ijetty.dispatcher;
 
 import org.apache.commons.lang3.StringUtils;
-import org.glassfish.hk2.api.DynamicConfiguration;
-import org.glassfish.hk2.api.DynamicConfigurationService;
-import org.glassfish.hk2.api.ServiceLocator;
-import org.glassfish.hk2.api.ServiceLocatorFactory;
-import org.glassfish.hk2.utilities.Binder;
-import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
-import org.glassfish.jersey.internal.AbstractRuntimeDelegate;
-import org.glassfish.jersey.message.internal.MessagingBinders;
-import org.glassfish.jersey.server.ContainerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
-import org.jvnet.hk2.external.generator.ServiceLocatorGeneratorImpl;
-import org.mortbay.ijetty.resources.HelloResource;
 
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ext.RuntimeDelegate;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Enumeration;
 
 public class ApplicationDispatcherServlet extends HttpServlet {
-    private ServletContainer servletContainer;
-
-    private static class CustomRuntimeDelegate extends AbstractRuntimeDelegate {
-        protected CustomRuntimeDelegate(ServiceLocator serviceLocator) {
-//            super(Injections.createLocator("my-jersey-common-rd-locator", new MessagingBinders.HeaderDelegateProviders()));
-            super(serviceLocator);
-        }
-
-        @Override
-        public <T> T createEndpoint(Application application, Class<T> endpointType)
-                throws IllegalArgumentException, UnsupportedOperationException {
-            if (application == null) {
-                throw new IllegalArgumentException("application is null.");
-            }
-            return ContainerFactory.createContainer(endpointType, application);
-        }
-    }
-
 
     @Override
-    public void init() throws ServletException {
+    public void init(ServletConfig config) throws ServletException {
         super.init();
-
-        ServiceLocatorFactory factory = ServiceLocatorFactory.getInstance();
-        ServiceLocator locator = factory.create("my-jersey-common-rd-locator", null, new ServiceLocatorGeneratorImpl(),
-                ServiceLocatorFactory.CreatePolicy.DESTROY);
-        locator.setNeutralContextClassLoader(false);
-        ServiceLocatorUtilities.enablePerThreadScope(locator);
-        bind(locator, new MessagingBinders.HeaderDelegateProviders());
-
-        RuntimeDelegate delegate = new CustomRuntimeDelegate(locator);
-        RuntimeDelegate.setInstance(delegate);
-
-        ResourceConfig resourceConfig = new ResourceConfig();
-        resourceConfig.register(HelloResource.class);
-        servletContainer = new ServletContainer(resourceConfig);
-
-//        ServletConfig servletConfig = new ServletConfig() {
-//            @Override
-//            public String getServletName() {
-//                return "ApplicationDispatcherServlet";
-//            }
-//
-//            @Override
-//            public ServletContext getServletContext() {
-//                return ApplicationDispatcherServlet.this.getServletContext();
-//            }
-//
-//            @Override
-//            public String getInitParameter(String name) {
-//                return null;
-//            }
-//
-//            @Override
-//            public Enumeration<String> getInitParameterNames() {
-//                return Collections.emptyEnumeration();
-//            }
-//        };
-//        servletContainer.init(servletConfig);
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        servletContainer.service(request, response);
+
         response.setContentType("text/html");
         ServletOutputStream out = response.getOutputStream();
         out.println("<html>");
         out.println("<h1>Hello 4 From Servlet Land! " + StringUtils.capitalize("hi") + "</h1>");
         out.println("</html>");
         out.flush();
-    }
-
-    private static void bind(final ServiceLocator locator, final Binder binder) {
-        final DynamicConfigurationService dcs = locator.getService(DynamicConfigurationService.class);
-        final DynamicConfiguration dc = dcs.createDynamicConfiguration();
-
-        locator.inject(binder);
-        binder.bind(dc);
-
-        dc.commit();
     }
 }
--- webapp-parent/web-app/src/main/java/org/mortbay/ijetty/hello/HelloWorld.java
+++ webapp-parent/web-app/src/main/java/org/mortbay/ijetty/hello/HelloWorld.java
@@ -1,9 +1,6 @@
 package org.mortbay.ijetty.hello;
 
 import org.apache.commons.lang3.StringUtils;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
-import org.mortbay.ijetty.resources.HelloResource;
 
 import java.io.IOException;
 
@@ -22,7 +19,6 @@ import javax.servlet.http.HttpServletResponse;
  * Hello Servlet
  */
 public class HelloWorld extends HttpServlet {
-    private ServletContainer servletContainer;
     String proofOfLife = null;
 
     /* ------------------------------------------------------------ */
@@ -33,11 +29,6 @@ public class HelloWorld extends HttpServlet {
         android.content.ContentResolver resolver = (android.content.ContentResolver) o;
         android.content.Context androidContext = (android.content.Context) config.getServletContext().getAttribute("com.ipostu.androidjettycontainer.context");
         proofOfLife = androidContext.getApplicationInfo().packageName;
-        org.glassfish.hk2.utilities.Binder A;
-
-        ResourceConfig resourceConfig = new ResourceConfig();
-        resourceConfig.register(HelloResource.class);
-        servletContainer = new ServletContainer(resourceConfig);
     }
 
     /* ------------------------------------------------------------ */
@@ -47,15 +38,13 @@ public class HelloWorld extends HttpServlet {
 
     /* ------------------------------------------------------------ */
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        servletContainer.service(request, response);
-
-//        response.setContentType("text/html");
-//        ServletOutputStream out = response.getOutputStream();
-//        out.println("<html>");
-//        out.println("<h1>Hello 3 From Servlet Land! " + StringUtils.capitalize("hi") + "</h1>");
-//        out.println("Brought to you by: " + proofOfLife);
-//        out.println("</html>");
-//        out.flush();
+        response.setContentType("text/html");
+        ServletOutputStream out = response.getOutputStream();
+        out.println("<html>");
+        out.println("<h1>Hello 3 From Servlet Land! " + StringUtils.capitalize("hi") + "</h1>");
+        out.println("Brought to you by: " + proofOfLife);
+        out.println("</html>");
+        out.flush();
     }
 
 }
