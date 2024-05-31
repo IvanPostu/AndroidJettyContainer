224e047b360730d6e9d7068699e718c0e12fc440
ivan <ipostu20000127@gmail.com>
2024-05-31 17:59:08 +0300

    Add 2 fragments, navigation and animation

--- AndroidJettyContainer/app/src/main/java/com/ipostu/androidjettycontainer/FirstFragment.kt
+++ AndroidJettyContainer/app/src/main/java/com/ipostu/androidjettycontainer/FirstFragment.kt
@@ -5,40 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.ipostu.androidjettycontainer.databinding.FragmentFirstBinding
+import android.widget.TextView
+import androidx.navigation.Navigation
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var textView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        textView = view.findViewById(R.id.textView1)
+        textView.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.navigateToSecondFragment)
         }
+
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
--- AndroidJettyContainer/app/src/main/java/com/ipostu/androidjettycontainer/SecondFragment.kt
+++ AndroidJettyContainer/app/src/main/java/com/ipostu/androidjettycontainer/SecondFragment.kt
@@ -5,40 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.ipostu.androidjettycontainer.databinding.FragmentSecondBinding
+import android.widget.TextView
+import androidx.navigation.Navigation
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SecondFragment : Fragment() {
-
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var textView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        textView = view.findViewById(R.id.textView1)
+        textView.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.navigateToFirstFragment)
         }
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return view
     }
 }
