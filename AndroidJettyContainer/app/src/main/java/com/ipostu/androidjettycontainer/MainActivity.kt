72c606edb86ade7e4749bb3e6bd2bc517df87d62
ivan <ipostu20000127@gmail.com>
2024-06-01 11:56:23 +0300

    temp

--- AndroidJettyContainer/app/src/main/java/com/ipostu/androidjettycontainer/MainActivity.kt
+++ AndroidJettyContainer/app/src/main/java/com/ipostu/androidjettycontainer/MainActivity.kt
@@ -1,17 +1,44 @@
 package com.ipostu.androidjettycontainer
 
 import android.os.Bundle
+import android.widget.Toolbar
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.ipostu.androidjettycontainer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationView: NavigationView
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController()
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+        navigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun findNavController(): NavController {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        return navHostFragment.navController
     }
 }
